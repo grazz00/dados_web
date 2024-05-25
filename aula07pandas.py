ae9ac3fe27dec50853d616068bbffd2ae0cd6cdc
Grazz <144062966+grazz00@users.noreply.github.com>
2024-05-25 12:21:40 -0300

    Add files via upload

---
+++ aula07pandas.py
@@ -0,0 +1,6 @@
+## biblioteca panda >> extrair arquivos de csv 
+## importando biblioteca pandas
+import pandas as pd
+
+#google colab >> jupiter nottebok >> tradutores do codigos >> trabalhando com dados >> executa o codigo uma unica vez conseguimos executar o codigo
+#por bloco
---
+++ aula_18.py
@@ -0,0 +1,39 @@
+# # numero positivo ou negativo
+
+# def positivoOuNegtivo(numero):
+   
+#     if numero > 0:
+#         return f'{numero} é um valor postivo'
+#     elif numero <0:
+#         return f'{numero} é um valor negativo'
+#     else:
+#         return f'{numero} é neutro'
+    
+# print(positivoOuNegtivo(-5))
+# print(positivoOuNegtivo(50))
+# print(positivoOuNegtivo(0))
+    
+
+
+
+
+#     Variaveis globais(todas as funcoes tem acesso) e locais (somente a função na qual ela é criada, conhece)
+#     # quando utulizamos a função para tirar o erro colocamos o except e o try
+
+
+# valor_01 = 0 # variavel global
+
+# def soma():
+#     a = 0 # variavel local
+#     return valor_01 + a
+
+# def div(b): # variavael de parametro mas que tem similariedade local
+#     try:
+#         return valor_01/ b
+#     except: # o except vai tratar as excessoes,os casos de erro.
+#         return 'erro: não foi possivel dividir'
+# # print(div(0))
+
+    
+    
+
